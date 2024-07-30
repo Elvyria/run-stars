6cb4c56f03f34ccdd28959c2a5ad6d8b8cd7053c
Elvyria <elvyria@tutanota.com>
2024-07-30 03:26:57 +0000

    added useful panic messages to xdg functions

--- src/xdg.rs
+++ src/xdg.rs
@@ -6,14 +6,14 @@ pub fn home() -> String {
     }
 
     let uid = Some(rustix::process::geteuid().as_raw().to_string());
-    let passwd = File::open("/etc/passwd").unwrap();
+    let passwd = File::open("/etc/passwd").expect("/etc/passwd should be accessable if $HOME is not set");
 
     for l in std::io::BufReader::new(passwd).lines() {
-        let l = l.unwrap();
+        let l = l.expect("reading /etc/passwd should not cause issues");
         let mut parts = l.split(':');
 
         if uid.as_deref() == parts.nth(2) {
-            return parts.nth(2).map(str::to_owned).unwrap();
+            return parts.nth(2).map(str::to_owned).expect("/etc/passwd should not be malformed");
         }
     }
 
